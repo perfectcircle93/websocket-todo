5069ca75f0132c87fc9917af1d48e16ca0532aab
Magdalena Świderska <m.swiderska@outlook.com>
2020-10-21 17:01:04 +0200

    Add socket io

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const socket = require('socket.io');
+
+let tasks = [];
 
 const app = express();
 
@@ -9,3 +12,19 @@ const server = app.listen(process.env.PORT || 8000, () => {
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
+
+
+
+const io = socket(server);
+
+io.on('connection', () => {
+    socket.emit('updateData', tasks);
+    socket.on('addTask', (newTask) => {
+      tasks.push(newTask);
+      socket.broadcast.emit('addTask', newTask);
+    });
+    socket.on('removeTask', (idTask) => {
+      tasks.splice(idTask);
+      socket.broadcast.emit('removeTask', idTask);
+    });
+  }); 
